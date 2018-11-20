1cabd3d3ed7ac954b02fe378eadeda571a70afb6
Dan Rusu <dan.rusu@azets.com>
2018-11-20 15:39:51 +0200

    ait seminar 2

--- src/projects/danrusu/testCases/CalculateApi.java
+++ src/projects/danrusu/testCases/CalculateApi.java
@@ -11,94 +11,83 @@ import base.testCase.TestCase;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 
-
 public class CalculateApi extends TestCase {
 
-	
 	private final String API_CALCULATE = "http://danrusu.ro/api/calculate.php";
-	
-	
-	private enum Operations{
-		
+
+	private enum Operations {
+
 		SUM(1),
-		
+
 		MULTIPLICATION(2),
-		
+
 		DIVISION(3);
-		
-		
-		Operations(int value){
+
+		Operations(int value) {
 			this.value = value;
 		}
-		
-		
+
 		private int value;
-		
-		
+
 		public int getValue() {
 			return value;
 		}
 	}
-	
-	
+
 	@Override
 	public void run() {
-		
-		
+
 		String firstNumber = evalAttribute("firstNumber");
-		
+
 		String secondNumber = evalAttribute("secondNumber");
-		
-		String operation = "" + Operations
-				.valueOf(evalAttribute("operation").toUpperCase())
-				.getValue();
-		
-		String expectedResult = evalAttribute("expectedResult");  
-		
-		
-		String finalUrl = addToUrl(API_CALCULATE, 
-				"firstNumber=" + firstNumber, 
-				"secondNumber=" + secondNumber, 
+
+		String operation = "" + Operations.valueOf(evalAttribute("operation").toUpperCase()).getValue();
+
+		String expectedResult = evalAttribute("expectedResult");
+
+		String finalUrl = addToUrl(API_CALCULATE, "firstNumber=" + firstNumber, "secondNumber=" + secondNumber,
 				"operation=" + operation);
-		
-				
-		log("URL:" +  finalUrl);
-		
-		Response response = request(GET, finalUrl);		
-		
-		log("Response body: " + response.getBody().asString());
+
+		log("URL:" + finalUrl);
+
+		Response response = request(GET, finalUrl);
+
+		String responseBody = response.getBody().asString();
+		log("Response body: " + responseBody);
 		log("Response headers:\n" + response.getHeaders());
-		
-		
+
 		ValidatableResponse validatableResponse = response.then();
 		
+		
+//		ThrowablesWrapper.unchecked("Body: " + responseBody,
+//
+//				() -> {
 
+		
 		// Validation (each throws if failure)
 		validatableResponse.statusCode(200);
-		
-		validatableResponse.header("Content-type", is("application/json"));		
-        
+
+		validatableResponse.header("Content-type", is("application/json"));
+
 		validatableResponse.body(
-				
-        			"numbers", hasSize(2),
-        			"numbers", hasItems(firstNumber, secondNumber),
-        			
-        			"operation", is(operation));
-        			
-		
-		validatableResponse.body("result", is(expectedResult));  
+
+				"numbers", hasSize(2), "numbers", hasItems(firstNumber, secondNumber),
+
+				"operation", is(operation));
+
+		validatableResponse.body("result", is(expectedResult));
+//					return null;
+//		});
 	}
 
-	
-	private String addToUrl(String url, String ...more) {
-		
+	private String addToUrl(String url, String... more) {
+
 		return url + "?" + String.join("&", more);
 	}
-	
 
 	@Override
 	public String getTestCaseScenario() {
-		
+
 		return newScenario("Validate API: " + API_CALCULATE);
 	}
 
