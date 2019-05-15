5efd4f8b6df56e1e873a90dbf7966b8f2806ab76
Dan Rusu <danginkgo@yahoo.com>
2019-05-15 09:45:15 +0300

    assertions fix

--- src/main/java/base/Assert.java
+++ src/main/java/base/Assert.java
@@ -1,8 +1,8 @@
 package main.java.base;
 
 
-import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 import static main.java.base.Assert.AssertCount.assertCount;
 import static main.java.base.Logger.log;
 import static main.java.base.Logger.logSplitByLines;
@@ -18,340 +18,322 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import org.junit.jupiter.api.Assertions;
-
 
 public interface Assert{
-    
-    
-    public static class AssertCount{
-        public static int assertCount = 0; 
-    }
 
 
-    private static void pass(
-            String assertionDescription,
-            String expected) {
-
-        assertCount++;
-        
-        log(assertionDescription
-                + " expected: \"" + expected + "\" "
-                + "SUCCEEDED!");
-    }
+	public static class AssertCount{
+		public static int assertCount = 0; 
+	}
 
 
+	private static void pass(
+			String assertionDescription,
+			String expected) {
 
-    private static void fail(
-            String assertionDescription,
-            String expected, 
-            String actual){
-        
-        assertCount++;
-        
-        String failureMessage = String.join(" | ",
-                assertionDescription,
-                "expected: \"" + expected + "\"",
-                "actual: \"" + actual + "\"",
-                "FAILED"); 
-  
-        logSplitByLines(failureMessage);
+		assertCount++;
 
-        throw new AssertionError(failureMessage);
-    }
+		log(assertionDescription
+				+ " expected: \"" + expected + "\" "
+				+ "SUCCEEDED!");
+	}
 
 
 
-    public static <T> void isCondition(
-            String assertionDescription,
-            T expected,
-            T actual,
-            BiPredicate<T, T> condition) {
+	private static void fail(
+			String assertionDescription,
+			String expected, 
+			String actual){
 
-        if (condition.test(
-                
-                Optional.ofNullable(expected)
-                    .orElseThrow(() -> new AssertionError("Expected value is null!")),
-                
-                Optional.ofNullable(actual)
-                    .orElseThrow(() -> new AssertionError("Actual value is null!")))){
-            
-            pass(assertionDescription, expected.toString());
-        }
-        else {
-            fail(assertionDescription, expected.toString(), actual.toString());
-        }
-    }
+		assertCount++;
 
-    
-    public static <T> void isConditionQuiet(
-            String assertionDescription,
-            T expected,
-            T actual,
-            BiPredicate<T, T> condition) {
+		String failureMessage = String.join(" | ",
+				assertionDescription,
+				"expected: \"" + expected + "\"",
+				"actual: \"" + actual + "\"",
+				"FAILED"); 
 
-        if (! condition.test(
-                
-                Optional.ofNullable(expected)
-                    .orElseThrow(() -> new AssertionError("Expected value is null!")),
-                    
-                Optional.ofNullable(actual)
-                    .orElseThrow(() -> new AssertionError("Actual value is null!")))){
-            
-            fail(assertionDescription, expected.toString(), actual.toString());
-        }
-    }
-    
-    
-    public static Function<Runnable, AssertionError> runnableToAssertionErrorOrNull = runnable -> {
-        
-        try {
-            runnable.run();
-        }
-        catch(AssertionError ae) {
-            return ae;
-        }
-        
-        return null;
-    };
-    
-     
-    public static List<AssertionError> getAssertionErrors(Runnable ...assertions) {
+		logSplitByLines(failureMessage);
 
-        return getAssertionErrors(List.of(assertions));
-            
-    }
+		throw new AssertionError(failureMessage);
+	}
 
-    
-    public static List<AssertionError> getAssertionErrors(List<Runnable> assertionsList) {
 
-        Predicate<AssertionError> assertionIsNotNul = assertionError -> assertionError != null;
-        
+
+	public static <T> void isCondition(
+			String assertionDescription,
+			T expected,
+			T actual,
+			BiPredicate<T, T> condition) {
+
+		if (condition.test(
+
+				Optional.ofNullable(expected)
+				.orElseThrow(() -> new AssertionError("Expected value is null!")),
+
+				Optional.ofNullable(actual)
+				.orElseThrow(() -> new AssertionError("Actual value is null!")))){
+
+			pass(assertionDescription, expected.toString());
+		}
+		else {
+			fail(assertionDescription, expected.toString(), actual.toString());
+		}
+	}
+
+
+	public static <T> void isConditionQuiet(
+			String assertionDescription,
+			T expected,
+			T actual,
+			BiPredicate<T, T> condition) {
+
+		if (! condition.test(
+
+				Optional.ofNullable(expected)
+				.orElseThrow(() -> new AssertionError("Expected value is null!")),
+
+				Optional.ofNullable(actual)
+				.orElseThrow(() -> new AssertionError("Actual value is null!")))){
+
+			fail(assertionDescription, expected.toString(), actual.toString());
+		}
+	}
+
+
+	public static Function<Runnable, AssertionError> runnableToAssertionErrorOrNull = runnable -> {
+
+		try {
+			runnable.run();
+		}
+		catch(AssertionError ae) {
+			return ae;
+		}
+
+		return null;
+	};
+
+
+	public static List<AssertionError> getAssertionErrors(Runnable ...assertions) {
+
+		return getAssertionErrors(List.of(assertions));
+
+	}
+
+
+	public static List<AssertionError> getAssertionErrors(List<Runnable> assertionsList) {
+
+		Predicate<AssertionError> assertionIsNotNul = assertionError -> assertionError != null;
+
 		return assertionsList.stream()
-                .map(runnableToAssertionErrorOrNull)
-                .filter(assertionIsNotNul)
-                .collect(toList());
-    }
-
-    
-    public static void verifyAll(List<AssertionError> assertionErrors) {
-                
-        if (! assertionErrors.isEmpty()) {      
-            
-            String allAssertionErrors = assertionErrors.stream().unordered()
-                    
-                    .map(AssertionError::getMessage)
-                    
-                    .collect(joining("\n"));
-            
-            throw new AssertionError(allAssertionErrors);
-        }
-    }
-       
-    
-    public static void softlyAssertAll(Runnable ...assertions) {
-        
-        verifyAll(getAssertionErrors(assertions));
-    }
-
-    
-    public static <T> void isEqual(
-            T expected,            
-            T actual){
-
-        isEqual(expected, actual, "");
-    }
-       
-    
-    public static <T> void isEqual(
-            T expected,
-            T actual,            
-            String assertionDescription){
-
-        isCondition(assertionDescription, 
-                expected, 
-                actual, 
-                (exp, act) -> exp.equals(act));
-    }
-    
-    
-    public static <T> void isEqualQuiet(
-            String assertionDescription,
-            T expected,            
-            T actual){
-
-        isConditionQuiet(assertionDescription, 
-                expected, 
-                actual, 
-                (exp, act) -> exp.equals(act));
-    }
-    
-    
-    public static void isEqualIgnoreCase(
-            String assertionDescription,
-            String expected,
-            String actual){
-
-        isCondition(assertionDescription, 
-                expected, 
-                actual, 
-                (exp, act) -> exp.equalsIgnoreCase(act));
-    }
-    
-    
-    public static void isEqualIgnoreCase(
-            String expected,
-            String actual){
-
-        isEqualIgnoreCase("", 
-                expected, 
-                actual);
-    }
-    
-    
-    public static void matchesRegex(
-            String assertionDescription,
-            String expectedRegex,
-            String actual){
-
-        isCondition(assertionDescription, 
-                expectedRegex, 
-                actual, 
-                (exp, act) -> act.matches(expectedRegex));
-    }
+				.map(runnableToAssertionErrorOrNull)
+				.filter(assertionIsNotNul)
+				.collect(toList());
+	}
 
 
-    public static void isEqualAsFloat(
-            String assertionDescription,
-            String expected,
-            String actual){
+	public static void verifyAll(List<AssertionError> assertionErrors) {
 
-        isCondition(assertionDescription, 
-                expected.replace(",", ""), 
-                actual, 
-                (exp, act) -> Float.parseFloat(exp) == Float.parseFloat(act));
-    }
+		if (! assertionErrors.isEmpty()) {      
 
+			String allAssertionErrors = assertionErrors.stream().unordered()
 
-    public static void actualContainsExpected(
-            String assertionDescription,
-            String expected,
-            String actual){
+					.map(AssertionError::getMessage)
 
-        isCondition(assertionDescription, 
-                expected, 
-                actual, 
-                (exp, act) -> act.contains(exp));
-    }
+					.collect(joining("\n"));
+
+			throw new AssertionError(allAssertionErrors);
+		}
+	}
 
 
-    public static void expectedContainsActual(
-            String assertionDescription,
-            String expected,
-            String actual){
+	public static void softlyAssertAll(Runnable ...assertions) {
 
-        actualContainsExpected(
-                assertionDescription, 
-                actual, 
-                expected);
-    }
+		verifyAll(getAssertionErrors(assertions));
+	}
 
 
-    public static void fail(String failureMessage){
-        
-        logSplitByLines("Assertion failed!" + failureMessage);
-        throw new AssertionError(failureMessage);
-    }
+	public static <T> void isEqual(
+			T expected,            
+			T actual){
 
-    
-    public static <T> void assertList(List<T> expectedList, List<T> actualList){
-        
-        isEqual(expectedList.size(), actualList.size(), "Verify list size");
-        
-        List<Runnable> assertionsList = IntStream.range(0, expectedList.size()-1)
-                
-            .mapToObj(i -> (Runnable)() -> isEqual(
-                    
-                    expectedList.get(i).toString(),
-                    
-                    actualList.get(i).toString()))
-            
-            .collect(Collectors.toList());
-                
-        verifyAll(getAssertionErrors(assertionsList));
-     }
-    
-    
-    public static <T> void assertListAnyOrder(List<T> expectedList, List<T> actualList){
-        
-        isEqual(expectedList.size(), actualList.size(), "Verify list size");
-        
-        List<Runnable> assertionsList = IntStream.range(0, expectedList.size()-1)
-                
-            .mapToObj(i -> (Runnable)() -> Assertions.assertTrue(
-                    
-                    expectedList.contains(actualList.get(i))))
-            
-            .collect(toList());
-                
-        verifyAll(getAssertionErrors(assertionsList));
-     }
-   
-    
-    public static <T, U> void assertMap(Map<T, U> expectedMap, Map<T, U> actualMap){
-        
-        isEqual(expectedMap.size(), actualMap.size(), "Verify maps size");
-        
-        List<Runnable> assertionsList = expectedMap.keySet().stream()
-                
-            .map(key ->
-            
-                (Runnable)() -> isEqual(
-                        
-                    key + ":" + expectedMap.get(key),
-                    
-                    key + ":" + actualMap.get(key)))
-            
-            .collect(Collectors.toList());
-                
-        verifyAll(getAssertionErrors(assertionsList));
-     }
+		isEqual(expected, actual, "");
+	}
 
 
-    static void customAssertString(
-            String assertionDescription, 
-            String expected, 
-            String actual, 
-            AssertCondition assertCondition) {
-        
-        isCondition(assertionDescription, 
-                expected, 
-                actual, 
-                assertCondition.getCondition());
-        
-    }
-    
-    public static <T> void conditionalAssert(       
-            
-            Predicate<T> condition,
-            
-            String message,
-            T expected,
-            T actual,
-            
-            BiConsumer<T, T> assertionIfCondition,
-            BiConsumer<T, T> assertionIfNotCondition) {
-            
-    	executeUnchecked(
-    			() -> {
-    				if (condition.test(actual)){           
-    					assertionIfCondition.accept(expected, actual);
-    				}
-    				else {            
-    					assertionIfNotCondition.accept(expected, actual);            
-    				}
-    			},
-    			
-    			message);
-    }
+	public static <T> void isEqual(
+			T expected,
+			T actual,            
+			String assertionDescription){
+
+		isCondition(assertionDescription, 
+				expected, 
+				actual, 
+				(exp, act) -> exp.equals(act));
+	}
+
+
+	public static <T> void isEqualQuiet(
+			String assertionDescription,
+			T expected,            
+			T actual){
+
+		isConditionQuiet(assertionDescription, 
+				expected, 
+				actual, 
+				(exp, act) -> exp.equals(act));
+	}
+
+
+	public static void isEqualIgnoreCase(
+			String assertionDescription,
+			String expected,
+			String actual){
+
+		isCondition(assertionDescription, 
+				expected, 
+				actual, 
+				(exp, act) -> exp.equalsIgnoreCase(act));
+	}
+
+
+	public static void isEqualIgnoreCase(
+			String expected,
+			String actual){
+
+		isEqualIgnoreCase("", 
+				expected, 
+				actual);
+	}
+
+
+	public static void matchesRegex(
+			String assertionDescription,
+			String expectedRegex,
+			String actual){
+
+		isCondition(assertionDescription, 
+				expectedRegex, 
+				actual, 
+				(exp, act) -> act.matches(expectedRegex));
+	}
+
+
+	public static void isEqualAsFloat(
+			String assertionDescription,
+			String expected,
+			String actual){
+
+		isCondition(assertionDescription, 
+				expected.replace(",", ""), 
+				actual, 
+				(exp, act) -> Float.parseFloat(exp) == Float.parseFloat(act));
+	}
+
+
+	public static void actualContainsExpected(
+			String assertionDescription,
+			String expected,
+			String actual){
+
+		isCondition(assertionDescription, 
+				expected, 
+				actual, 
+				(exp, act) -> act.contains(exp));
+	}
+
+
+	public static void expectedContainsActual(
+			String assertionDescription,
+			String expected,
+			String actual){
+
+		actualContainsExpected(
+				assertionDescription, 
+				actual, 
+				expected);
+	}
+
+
+	public static void fail(String failureMessage){
+
+		logSplitByLines("Assertion failed!" + failureMessage);
+		throw new AssertionError(failureMessage);
+	}
+
+
+	public static <T> void assertList(List<T> expectedList, List<T> actualList){
+
+		isEqual(expectedList.size(), actualList.size(), "Verify list size");
+
+		List<Runnable> assertionsList = IntStream.range(0, expectedList.size()-1)
+
+				.mapToObj(i -> (Runnable)() -> isEqual(
+
+						expectedList.get(i).toString(),
+
+						actualList.get(i).toString()))
+
+				.collect(Collectors.toList());
+
+		verifyAll(getAssertionErrors(assertionsList));
+	}
+
+
+	public static <T, U> void assertMap(Map<T, U> expectedMap, Map<T, U> actualMap){
+
+		isEqual(expectedMap.size(), actualMap.size(), "Verify maps size");
+
+		List<Runnable> assertionsList = expectedMap.keySet().stream()
+
+				.map(key ->
+
+				(Runnable)() -> isEqual(
+
+						key + ":" + expectedMap.get(key),
+
+						key + ":" + actualMap.get(key)))
+
+				.collect(Collectors.toList());
+
+		verifyAll(getAssertionErrors(assertionsList));
+	}
+
+
+	static void customAssertString(
+			String assertionDescription, 
+			String expected, 
+			String actual, 
+			AssertCondition assertCondition) {
+
+		isCondition(assertionDescription, 
+				expected, 
+				actual, 
+				assertCondition.getCondition());
+
+	}
+
+	public static <T> void conditionalAssert(       
+
+			Predicate<T> condition,
+
+			String message,
+			T expected,
+			T actual,
+
+			BiConsumer<T, T> assertionIfCondition,
+			BiConsumer<T, T> assertionIfNotCondition) {
+
+		executeUnchecked(
+				() -> {
+					if (condition.test(actual)){           
+						assertionIfCondition.accept(expected, actual);
+					}
+					else {            
+						assertionIfNotCondition.accept(expected, actual);            
+					}
+				},
+
+				message);
+	}
 }
